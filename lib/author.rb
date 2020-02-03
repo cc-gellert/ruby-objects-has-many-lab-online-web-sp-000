f145f4d35b4b7de5b1b1efbbc8853bc9b756e527
Chloe Gell <gellert.chloe@gmail.com>
2020-02-03 20:49:17 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,21 @@
+class Author 
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+  end 
+  def posts 
+    Post.all.select {|post| post.author == self}
+  end 
+  def add_post(title)
+    new_post = Post.new(title)
+    new_post.author = self 
+  end 
+  def add_post_by_title(title)
+    new_post = Post.new(title)
+    new_post.author = self 
+  end 
+  def self.post_count 
+    postArr = Post.all.select {|post| post.author == self}
+    postArr.length 
+  end 
+end 
